67e7a2ba43953030b7b554afc58addbef80c234e
Sandro <sandromelo.sec@gmail.com>
2019-04-09 23:21:16 +0100

    09/04/2019-23:21:15 - First Commit

--- scripts/DEVEL/awsshell_v2.sh
+++ scripts/DEVEL/awsshell_v2.sh
@@ -1,12 +1,24 @@
+
 #!/bin/bash
 
+REGIONS="sa-east-1 us-east-1 us-west-1 us-west-2"
 
-IPNETWORK=$(aws ec2 describe-network-interfaces --query NetworkInterfaces[*].Association.PublicIp --output text)
+
+
+Func_network()
+{
+    aws ec2 describe-network-interfaces --query \
+    NetworkInterfaces[*].Association.PublicIp --output text) --region $1 \
+    --profile $2
+}
+           
+
 
 
 IPINSTANCES=$( aws ec2 describe-instances --output text |grep ^ASSOCIATION | grep -oE "\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" |sort |uniq )
 
 IPELASTICVPC=$( aws ec2 describe-addresses --filter Name=domain,Values=vpc --output json |grep PublicIp | awk '{ print $2}' | cut -f 2 -d \" | grep ^[0-9] )
+I
 func_listip()
 {
     for _IP in $( echo $1 )
@@ -15,18 +27,61 @@ func_listip()
     done
 }
 
-echo "...::: Network IP :::..."
-func_listip "$IPNETWORK"
-echo .
+
+func_geteach_ip()
+{
+    echo "...::: Network IP :::..."
+    func_listip "$IPNETWORK"
+    echo .
 
 
-echo "...::: INSTANCE IP :::..." 
-func_listip "$IPINSTANCES"
-echo .
+    echo "...::: INSTANCE IP :::..." 
+    func_listip "$IPINSTANCES"
+    echo .
 
 
-echo "...::: ELASTIC VPC IPs :::..." 
-func_listip "$IPELASTICVPC"
-echo .
+    echo "...::: ELASTIC VPC IPs :::..." 
+    func_listip "$IPELASTICVPC"
+    echo .
+}
+
+
+func_account()
+{
+
+    _MSGCOUNT="Coleta de informacoes da conta "
+
+    case  $_ACCOUNT in
+        greenbrasil)
+        echo "$_MSGCOUNT Greenbrasil"
+        echo"Green Brasil"
+        echo . 
+        ;;
+
+        greendevelop)
+        echo "$_MSGCOUNT Greendevelop"
+        echo"Desenvolvimento"
+        echo . 
+
+        ;;
+
+        greenhomolog)
+        echo "$_MSGCOUNT Greenhomolog"
+        echo"Homologacao"
+        echo . 
+
+        ;;
+
+        greenprod)
+        echo "$_MSGCOUNT Greenprod"
+        echo"Producao"
+        echo . 
+
+        ;;
+
+    esac
+
+}
+
 
 
