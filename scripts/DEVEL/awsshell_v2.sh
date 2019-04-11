3b28806b9cb844f65b587bb85e8c4c9899721525
Sandro <sandromelo.sec@gmail.com>
2019-04-11 16:22:43 +0100

    11/04/2019-16:22:43 - First Commit

--- scripts/DEVEL/awsshell_v2.sh
+++ scripts/DEVEL/awsshell_v2.sh
@@ -6,17 +6,21 @@ REGIONS="sa-east-1 us-east-1 us-west-1 us-west-2"
 AWS_ACCOUNTS="greendevelop"
 
 
-Func_network()
+func_network()
 {
-    aws ec2 describe-network-interfaces --query \
-    NetworkInterfaces[*].Association.PublicIp --output text) --region $1 \
-    --profile $2
+    aws ec2 describe-network-interfaces --query NetworkInterfaces[*].Association.PublicIp --output text --region $1 --profile $2
 }
            
+func_instances()
+{
+    IPINSTANCES=$( aws ec2 describe-instances --output text |grep ^ASSOCIATION | grep -oE "\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" |sort |uniq )
+}
 
-IPINSTANCES=$( aws ec2 describe-instances --output text |grep ^ASSOCIATION | grep -oE "\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" |sort |uniq )
+func_elastic()
+{
+    IPELASTICVPC=$( aws ec2 describe-addresses --filter Name=domain,Values=vpc --output json |grep PublicIp | awk '{ print $2}' | cut -f 2 -d \" | grep ^[0-9] )
+}
 
-IPELASTICVPC=$( aws ec2 describe-addresses --filter Name=domain,Values=vpc --output json |grep PublicIp | awk '{ print $2}' | cut -f 2 -d \" | grep ^[0-9] )
 
 func_listip()
 {
@@ -32,7 +36,7 @@ func_geteach_ip()
       do
         _PROFILE="$1"
         echo "...::: Network IP :::..."
-        func_listip $( Func_network $_REGION $_PROFILE )
+        func_listip $( func_network $_REGION $_PROFILE )
         echo .
     done
 
@@ -54,42 +58,48 @@ func_account()
 
     case  $_ACCOUNT in
         greenbrasil)
-        _PROFILE="checkip_br" 
-        echo "$_MSGCOUNT Greenbrasil"
-        echo"Green Brasil"
-        func_geteach_ip "$_PROFILE"
-        echo . 
+            _PROFILE="checkip_br" 
+            echo "$_MSGCOUNT Greenbrasil"
+            echo "Green Brasil"
+            func_geteach_ip "$_PROFILE"
+            echo . 
         ;;
 
         greendevelop)
-        _PROFILE="checkip_dv" 
-        echo "$_MSGCOUNT Greendevelop"
-        echo"Desenvolvimento"
-        echo . 
+            _PROFILE="checkip_dv" 
+            echo "$_MSGCOUNT Greendevelop"
+            echo "Desenvolvimento"
+            echo "RUNNING FUNC_ACCOUNT"
+            sleep 10s
+            func_geteach_ip "$_PROFILE"
+            echo . 
 
         ;;
 
         greenhomolog)
-        _PROFILE="checkip_hm" 
-        echo "$_MSGCOUNT Greenhomolog"
-        echo"Homologacao"
-        echo . 
+            _PROFILE="checkip_hm" 
+            echo "$_MSGCOUNT Greenhomolog"
+            echo "Homologacao"
+            echo . 
 
         ;;
 
         greenprod)
-        _PROFILE="checkip_pdv" 
-        echo "$_MSGCOUNT Greenprod"
-        echo"Producao"
-        echo . 
+            _PROFILE="checkip_pdv" 
+            echo "$_MSGCOUNT Greenprod"
+            echo "Producao"
+            echo . 
 
         ;;
 
+        *)
+            echo "Essa conta nao exite"
+        ;;
     esac
 }
 
-for _ACCOUNT in $(echo AWS_ACCOUNTS)
+for _ACCOUNT in $(echo $AWS_ACCOUNTS)
   do
-
+    func_account "$_ACCOUNT"
 
 done 
